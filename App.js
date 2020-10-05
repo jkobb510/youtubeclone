95f2be3674329f155824e2dd096a2e9f65ad8872
Seung Lee <41555063+jkobb510@users.noreply.github.com>
2020-10-05 19:00:55 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import { Grid } from '@material-ui/core';
+import  {SearchBar, VideoDetail, VideoList} from './Components';
+
+
+import youtube from './api/youtube';
+
+class App extends React.Component {
+    state = {
+        videos: [],
+        selectedVideo: null,
+    }
+
+componentDidMount(){
+    this.handleSubmit('pdf generation with react and node')
+}
+onVideoSelect = (video) => {
+    this.setState( {selectedVideo: video})
+}
+    handleSubmit = async (SearchTerm)=> {
+        const response = await youtube.get('search', {    
+            params: {
+        part: 'snippet', 
+        maxResults: 5, 
+        key: 'AIzaSyD-T16zyYisn1DH38EdqIg57EAnps5tQvU',
+        q: SearchTerm, 
+        }
+                                                
+     });
+        this.setState({videos: response.data.items, selectedVideo: response.data.items[0]});
+    }
+render (){
+        const {selectedVideo, videos}=this.state;
+
+return (
+    <Grid justify="center" container spacing ={10}>
+        <Grid item xs = {12}>
+        <Grid container spacing = {10}>
+            <Grid item xs={12}>
+                <SearchBar onFormSubmit={this.handleSubmit} />
+
+        </Grid>
+        <Grid item xs={8}>
+            <VideoDetail video={selectedVideo} />
+        </Grid>
+        <Grid item xs={4}>
+            <VideoList videos = {videos} onVideoSelect={this.onVideoSelect}/>
+
+    
+    </Grid>
+    </Grid>
+    </Grid>
+    </Grid>
+    )
+}
+}
+
+export default App;
